0706c7fc32c501da26b31b6830c471585a7db42f
Luis Flores <me@luisflores.mx>
2014-04-07 19:32:14 +0000

    Initial Commit

---
+++ fly-butterfly/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  fly-butterfly
+//
+
+//  Copyright (c) 2014 Iguana Studios. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ fly-butterfly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fly-butterfly
+//
+//  Created by Luis Flores on 4/7/14.
+//  Copyright (c) 2014 Iguana Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
